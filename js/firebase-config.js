74077dad95a164de4add2bf70cc724ae30f51e11
leporeneto <neto.lepore@gmail.com>
2025-06-19 12:08:46 -0300

    criado firebase-config.js

--- js/firebase-config.js
+++ js/firebase-config.js
@@ -1,1 +1,21 @@
-// Adicione aqui sua configuração Firebase
+// Adicione aqui sua configuração Firebase<!-- Firebase SDK -->
+<script type="module">
+  // Importações diretas do SDK via CDN
+  import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-app.js";
+  import { getFirestore } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-firestore.js";
+
+  const firebaseConfig = {
+    apiKey: "AIzaSyB70N-A-khTjspEq1pGv7E02uo19qdbppU",
+    authDomain: "glicemia-tracker-660fa.firebaseapp.com",
+    projectId: "glicemia-tracker-660fa",
+    storageBucket: "glicemia-tracker-660fa.firebasestorage.app",
+    messagingSenderId: "745044970805",
+    appId: "1:745044970805:web:fc85e2485c7a98381a507c"
+  };
+
+  const app = initializeApp(firebaseConfig);
+  const db = getFirestore(app);
+
+  // Disponibiliza o banco globalmente
+  window.db = db;
+</script>
